89ce0eb396e36603824b723c2b7fb1923827d2b3
Kevin Tsui <iceprincefounder@qq.com>
2017-03-12 01:42:16 +0800

    add unreal project

---
+++ unreal/project/Source/project/projectGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "projectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_API AprojectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
